07c1f524f3d03355f603ad70c28690f7a75feddc
steveodhiambo <112956208+steve601@users.noreply.github.com>
2024-07-14 21:36:21 +0300

    docker image commit

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Credit Card Spending Habits
-## Problem statement
+# DIABETES DIAGNOSIS
 
-**Problem**
+## Requirements
+Flask
+Dockerhub account
+MLflow
+AWS ec2 account
 
-With a variety in variables, researchers have an opportunity to uncover deeper trends in customer spending as well as interesting correlations between data points that can serve as invaluable business intelligence. Whether you're interested in learning more about customer preferences or simply exploring unbiased data analysis techniques, this data is sure to provide insight beyond what one could anticipate.The main objective of this project is to develop a predicitive machine learning model,that is able to predict the amount of transaction that can be made by a customer.
+### Overview
 
-The model is serialized,then we create a web application using Flask, then deploy the application to AWS EC2 instance.
+Predicts whether a patient has diabetes or not.
+
+The application is packed and run as a container,then pushed to the DockerHub
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+def load_object(file_path):
+    with open(file_path, 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
+
+model_path = 'models/model.pkl'
+scaler_path ='models/scaler.pkl'
+
+model = load_object(model_path)
+scaler = load_object(scaler_path)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def prediction():
+    x = [i for i in request.form.values()]
+    
+    columns = ['pregnancies', 'glucose', 'bloodpressure', 'skinthickness', 'insulin','bmi', 'diabetespedigreefunction', 'age']
+    x = pd.DataFrame([x],columns=columns)
+    
+    x = scaler.transform(x)
+    
+    y_pred = model.predict(x)
+    msg = 'Patient has diabetes' if y_pred == 1 else 'Patient has no diabetes'
+    
+    return render_template('index.html',text=msg)
+
+if __name__ == "__main__":
+    app.run(debug=True)
